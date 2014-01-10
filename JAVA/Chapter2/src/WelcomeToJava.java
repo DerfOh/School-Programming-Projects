1ce63b095c0a6cf24d5b9baa873599b0f89bbb3a
DerfOh <fpaulin@umflint.edu>
2014-01-10 14:01:59 -0500

    First programming example of the book
    just a basic welcome to Java program

---
+++ JAVA/Chapter2/src/WelcomeToJava.java
@@ -0,0 +1,13 @@
+//Fig. 2.1: Welcomem1.java
+//Text-Printing program.
+
+public class WelcomeToJava {
+
+	//main method begins execution of Java application
+	public static void main(String[] args) {//"main" indicates the method, void indicates nothing is returned, and "String[] args" is required too but wont be covered until later as to why
+		
+		System.out.println("Welcome to Java Programming!");//outputs to the standard output object called "System.out" 
+
+	}//end of main method
+
+}//end of class WelcomeToJava
