7d5a009912c5a8ace093539703258188d6aff8e9
DerfOh <fpaulin@umflint.edu>
2014-01-11 11:41:09 -0500

    Learning the printf method
    Showed me that you can format text using arguments %s representing a
    string. The order does matter with these, meaning the first %s
    corresponds to the first set of parenthesis and respectively the same
    for the second.

--- JAVA/Chapter2/src/WelcomeToJava.java
+++ JAVA/Chapter2/src/WelcomeToJava.java
@@ -6,7 +6,10 @@ public class WelcomeToJava {
 	//main method begins execution of Java application
 	public static void main(String[] args) {//"main" indicates the method, void indicates nothing is returned, and "String[] args" is required too but wont be covered until later as to why
 		
-		System.out.println("Welcome to\nto\nJava\nProgramming!");//outputs to the standard output object called "System.out" 
+		System.out.printf("%s\n%s\n", 
+				"Welcome to", "Java Programming!"); //printf method employed here
+													//printf means format
+
 	}//end of main method
 
 }//end of class WelcomeToJava
