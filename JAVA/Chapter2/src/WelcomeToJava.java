7c36a04574d4308f2c8167ec8dcc1ed0b86ba3ff
DerfOh <fpaulin@umflint.edu>
2014-01-11 11:21:51 -0500

    Learning the newline character
    Done so that we can observe that you need the "\n" character to place
    text on a new line.

--- JAVA/Chapter2/src/WelcomeToJava.java
+++ JAVA/Chapter2/src/WelcomeToJava.java
@@ -6,8 +6,8 @@ public class WelcomeToJava {
 	//main method begins execution of Java application
 	public static void main(String[] args) {//"main" indicates the method, void indicates nothing is returned, and "String[] args" is required too but wont be covered until later as to why
 		
-		System.out.println("Welcome to Java Programming!");//outputs to the standard output object called "System.out" 
-
+		System.out.println("Welcome to ");//outputs to the standard output object called "System.out" 
+		System.out.println("Java Programming!");
 	}//end of main method
 
 }//end of class WelcomeToJava
