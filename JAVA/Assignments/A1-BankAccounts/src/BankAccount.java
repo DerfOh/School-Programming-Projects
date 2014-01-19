32c0eb7c54f7a6bb91a5ea222b1b7fbaa5f59062
DerfOh <fpaulin@umflint.edu>
2014-01-19 12:12:47 -0500

    Moved main to a separate file
    Exploring how separate compilation works in Java, it seems a little
    friendlier than C

--- JAVA/Assignments/A1-BankAccounts/src/BankAccount.java
+++ JAVA/Assignments/A1-BankAccounts/src/BankAccount.java
@@ -120,7 +120,7 @@ public class BankAccount {
 		accountType = input.nextInt();
 		
 		if(accountType == 4){
-			main(null);
+			Driver.main(null);
 		}
 		
 		System.out.println("1. Withdraw money");
@@ -154,38 +154,6 @@ public class BankAccount {
 		}
 		System.exit(0);
 		
-	}
-	
-	
-	//main function of the program
-	public static void main(String[] args) {
-		
-		boolean verified;
-		System.out.println("Banking Program");
-		BankAccount account = new BankAccount();
-		int i;
-
-		//run the user through the login process. 
-		verified = account.login();
-		
-		for (i = 0; i<3;){
-			if (verified){
-				System.out.println("\nUser Verified.\n");
-				account.menu(account);
-			}
-			
-			else{
-				System.out.printf("\nUser Not Verified, attempt %d \n\n\n", i +1);
-				verified = account.login();
-				i++;
-			}
-		}
-		
-		System.out.printf("You have failed to log in %d times. Please contact your banker for assistance. The program will now close.", i);
-		System.exit(0);
-		
-		
-	
 	}
 
 }
