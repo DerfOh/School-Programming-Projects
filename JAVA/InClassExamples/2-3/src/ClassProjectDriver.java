5dfebcfe0a59b4bf1f8b5b608e426785cb15ad79
DerfOh <fredrick@fredrickpaulin.com>
2014-02-03 10:35:03 -0500

    Created a second object and a set method for new objects
    While you could probably build this function into the constructor for
    the sake of the example its useful to demonstrate it in main to show
    that you can work with a class from anywhere.

--- JAVA/InClassExamples/2-3/src/ClassProjectDriver.java
+++ JAVA/InClassExamples/2-3/src/ClassProjectDriver.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
 public class ClassProjectDriver {
 
-	public static void main(String[] args) {
-		
+	public static void setInfo(Person guy){
 		Scanner input = new Scanner(System.in);
-		Person peter = new Person();
 		int newAge = 0;
 		String newName = "";
-		
-		
 		System.out.println("Please enter the age: ");
 		newAge = input.nextInt();
 		
 		System.out.println("Please enter the name: ");
 		input.nextLine();
 		newName = input.nextLine();
+		guy.setAge(newAge);
+		guy.setName(newName);
+	}
+	
+	public static void main(String[] args) {
 		
-		peter.setAge(newAge);
-		peter.setName(newName);
+
+		Person peter = new Person();
+		Person jerry = new Person();
 		
+		setInfo(peter);
+		setInfo(jerry);
 		
 		System.out.printf("\n\n%s is %d years old.\n\n", peter.getName(), peter.getAge());
+		System.out.printf("\n\n%s is %d years old.\n\n", jerry.getName(), jerry.getAge());
+
 
 	}
 
