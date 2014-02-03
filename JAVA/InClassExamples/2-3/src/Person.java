b6a8c8c5f1fcc05757c91c25d07eaf16a9ce32f3
DerfOh <fredrick@fredrickpaulin.com>
2014-02-03 09:51:55 -0500

    Creating a new class in class for class
    Person class to demonstrate how to make a class.

---
+++ JAVA/InClassExamples/2-3/src/Person.java
@@ -0,0 +1,20 @@
+
+public class Person {
+	
+	private int age;
+	private String name;
+	
+	public void setAge(int x){
+		age = x;
+	}
+	public void setName(String x){
+		name = x;
+	}
+	public int getAge(){
+		return age;
+	}
+	public String getName(){
+		return name;
+	}
+	
+}
