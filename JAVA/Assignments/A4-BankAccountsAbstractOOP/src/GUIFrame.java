24425f8fe477d285dc21e085f217246dd146ef24
DerfOh <fpaulin@umflint.edu>
2014-04-21 02:45:01 -0400

    Finished UI
    Changed the layout to something that is much more manageable using
    gridBagLayout, made an option menu for transactions using JOptionPane,
    and many other functionality tweeks an implementations

--- JAVA/Assignments/A4-BankAccountsAbstractOOP/src/GUIFrame.java
+++ JAVA/Assignments/A4-BankAccountsAbstractOOP/src/GUIFrame.java
@@ -3,19 +3,28 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 
 public class GUIFrame extends JFrame{
+	//create BankAccount Objects
+	static BankAccount checking = new CheckingAccount(100, 2.50);//balance, fee
+	static BankAccount savings = new SavingsAccount(0.01, 200);//rate, balance
+	static BankAccount retirement = new RetirementAccount (0.25, 100, 1200);//rate, balance, annual income
+	
 	//initialize some labels 
 	private JLabel lbName;
 	private JLabel lbAction;
+	private JLabel lbAmount;
+	private JLabel lbBalance;
 	
 	//initialized text fields
 	private JTextField tfAmount;
-//	private JTextField tfAge;
+	private JTextField tfBalance;
 	
 	//create two buttons
-	private JButton btnCancel;
+	private JButton btnClear;
 	private JButton btnOK;
 	
 	//radio button
@@ -38,29 +47,34 @@ public class GUIFrame extends JFrame{
 		
 		//make it have a flow layout
 		super("Banking Application");
-		setLayout(new FlowLayout());//sets frame layout
+		setLayout(new GridBagLayout());//sets frame layout
+		GridBagConstraints c = new GridBagConstraints();
+		
 
 
 		//need to make labels real by creating actual objects
 		lbName = new JLabel("Account");
 		lbAction = new JLabel("Transaction");
+		lbAmount = new JLabel("Amount");
+		lbBalance = new JLabel("Balance");
 		
 		//add text fields
 		tfAmount = new JTextField(20);
-		tfAmount.setText("0.00");//can specify what will be in the field
+		tfBalance = new JTextField(20);
+		
 		
 		//make buttons
-		btnCancel = new JButton("Cancel");
+		btnClear = new JButton("Clear");
 		btnOK = new JButton("OK");
 		
 		//make radio buttons
-		rbSavings = new JRadioButton("Savings", true);//true indicates default option
+		rbSavings = new JRadioButton("Savings");
 		rbChecking = new JRadioButton("Checking");
 		rbRetirement = new JRadioButton("Retirement");
 		bgAccount = new ButtonGroup();
 		
 		//make transaction button groups
-		rbDeposite = new JRadioButton("Deposite", true);
+		rbDeposite = new JRadioButton("Deposite");
 		rbWithdraw = new JRadioButton("Withdraw");
 		rbTransfer = new JRadioButton("Transfer");
 		bgTransactions = new ButtonGroup();
@@ -78,40 +92,68 @@ public class GUIFrame extends JFrame{
 		
 		
 		//add the elements to the frame
-		add(lbName);
-		add(rbChecking);
-		add(rbSavings);
-		add(rbRetirement);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridy = 0;
+		c.gridx = 0;
+		add(lbName, c);
+		c.gridx = 1;
+		add(rbChecking, c);
+		c.gridx = 2;
+		add(rbSavings, c);
+		c.gridx = 3;
+		add(rbRetirement, c);
 		
 
 		//transactions
-		add(lbAction);
-		add(rbWithdraw);
-		add(rbDeposite);
-		add(rbTransfer);
+		c.gridy = 1;
+		c.gridx = 0;
+		add(lbAction, c);
+		c.gridx = 1;
+		add(rbWithdraw, c);
+		c.gridx = 2;
+		add(rbDeposite, c);
+		c.gridx = 3;
+		add(rbTransfer, c);
 		
-		
+		//balance
+		c.gridy = 2;
+		c.gridx = 0;
+		add(lbBalance, c);
+		c.gridx = 1;
+		add(tfBalance, c);
 
 		//Amount
-		add(tfAmount);
+		c.gridy = 3;
+		c.gridx = 0;
+		add(lbAmount, c);
+		c.gridx = 1;
+		add(tfAmount, c);
+		
+		
 		
 		//add confirmation buttons
-		add(btnCancel);
-		add(btnOK);
+		c.gridy = 4;
+		c.gridx = 0;
+		add(btnClear, c);
+		c.gridx = 1;
+		add(btnOK, c);
 		
 		
 		
 		//add action listeners
 		ButtonHandler btnHandler = new ButtonHandler();
-		btnCancel.addActionListener(btnHandler);
+		btnClear.addActionListener(btnHandler);
 		btnOK.addActionListener(btnHandler);
-		//rbChecking.addActionListener(itmListener);
 		
 		//add item listeners
-		ItemListener itmListener = new ItemListener();
-		rbChecking.addActionListener(itmListener);
+		RadioListener rbListener = new RadioListener();
+		rbChecking.addItemListener(rbListener);
+		rbSavings.addItemListener(rbListener);
+		rbRetirement.addItemListener(rbListener);
 		
-		
+		rbDeposite.addItemListener(rbListener);
+		rbWithdraw.addItemListener(rbListener);
+		rbTransfer.addItemListener(rbListener);
 	}
 
 	
@@ -123,11 +165,63 @@ public class GUIFrame extends JFrame{
 			if (event.getSource() == btnOK){//checks the source of the event 
 				
 				try{
-//					if(event.getSource() == rbChecking){
-//						JOptionPane.showMessageDialog(null, "Checking Selected.");
-//					}
 					double amount = Double.parseDouble(tfAmount.getText());
-					JOptionPane.showMessageDialog(null, amount);
+					if (rbChecking.isSelected()){
+						if (rbWithdraw.isSelected()){
+							MonetaryTransaction withdraw = new Withdraw(amount);
+							checking.doTransaction(withdraw);
+						}
+						else if (rbDeposite.isSelected()){
+							MonetaryTransaction deposit = new Deposit(amount);
+							checking.doTransaction(deposit);
+						}
+						else if (rbTransfer.isSelected()){
+							MonetaryTransaction withdraw = new Withdraw(amount);
+							checking.doTransaction(withdraw);
+							transferMenu(amount);
+						}
+						
+						else if (rbTransfer.isSelected()){
+							JOptionPane.showMessageDialog(null, "Please select the account to transfer the amount to by selecting it.");
+						}					
+						tfBalance.setText(String.valueOf(checking.getCurrentBalance()));
+					}
+					
+					else if (rbSavings.isSelected()){
+						if (rbWithdraw.isSelected()){
+							MonetaryTransaction withdraw = new Withdraw(amount);
+							savings.doTransaction(withdraw);
+						}
+						else if (rbDeposite.isSelected()){
+							MonetaryTransaction deposit = new Deposit(amount);
+							savings.doTransaction(deposit);
+						}
+						else if (rbTransfer.isSelected()){
+							MonetaryTransaction withdraw = new Withdraw(amount);
+							savings.doTransaction(withdraw);
+							transferMenu(amount);
+						}
+						tfBalance.setText(String.valueOf(savings.getCurrentBalance()));
+					}
+					
+					else if (rbRetirement.isSelected()){
+						if (rbWithdraw.isSelected()){
+							MonetaryTransaction withdraw = new Withdraw(amount);
+							retirement.doTransaction(withdraw);
+						}
+						else if (rbDeposite.isSelected()){
+							MonetaryTransaction deposit = new Deposit(amount);
+							retirement.doTransaction(deposit);
+						}
+						else if (rbTransfer.isSelected()){
+							MonetaryTransaction withdraw = new Withdraw(amount);
+							retirement.doTransaction(withdraw);
+							transferMenu(amount);
+						}
+						tfBalance.setText(String.valueOf(retirement.getCurrentBalance()));
+					}
+					
+					
 				}
 				catch(NumberFormatException e){
 					JOptionPane.showMessageDialog(null, "You should enter an integer.");
@@ -137,23 +231,23 @@ public class GUIFrame extends JFrame{
 				}
 			
 			}
-			else if (event.getSource() == btnCancel){
+			else if (event.getSource() == btnClear){
 				tfAmount.setText("0.00");
 				
 			}
 		}
 	}
 	
-	private class ItemListener implements ActionListener{
+	private class RadioListener implements ItemListener{
 
 		@Override
-		public void actionPerformed(ActionEvent event) {
+		public void itemStateChanged(ItemEvent event) {
 			
 			
 			if(event.getSource() == rbChecking){
 				try{
-					JOptionPane.showMessageDialog(null, "Checking Selected");
-					//fill out actions
+					//JOptionPane.showMessageDialog(null, "Checking Selected");
+					tfBalance.setText(String.valueOf(checking.getCurrentBalance()));
 				}
 				catch(Exception e){
 					JOptionPane.showMessageDialog(null,e);
@@ -164,8 +258,7 @@ public class GUIFrame extends JFrame{
 			
 			else if (event.getSource()==rbSavings){
 				try{
-					JOptionPane.showMessageDialog(null, "Savings Selected");
-						//fill out actions
+					tfBalance.setText(String.valueOf(savings.getCurrentBalance()));
 				}
 				catch(Exception e){
 					JOptionPane.showMessageDialog(null,e);
@@ -175,16 +268,58 @@ public class GUIFrame extends JFrame{
 			
 			else if (event.getSource()==rbRetirement){
 				try{
-					JOptionPane.showMessageDialog(null, "Retirement Selected");
-						
+					tfBalance.setText(String.valueOf(retirement.getCurrentBalance()));
 				}
 				catch(Exception e){
 					JOptionPane.showMessageDialog(null,e);
 				}
 			}
 			
+//			else if (event.getSource()==rbTransfer){
+//				try{
+//					double amount = Double.parseDouble(tfAmount.getText());
+//					
+//					
+//				}
+//				catch(Exception e){
+//					JOptionPane.showMessageDialog(null,e);
+//				}
+//			}
 			
-		
+
 		}
+	}
+	
+	public void transferMenu(double amount){
+		Object[] possibilities = {"Checking", "Savings", "Retirement"};
+		String s = (String)JOptionPane.showInputDialog(
+		                    null,
+		                    "Please choose the account to transfer the amount to...\n",
+		                    "Transfer Menu",
+		                    JOptionPane.PLAIN_MESSAGE,
+		                    null,
+		                    possibilities,
+		                    " ");
+		if (s == "Checking"){
+			MonetaryTransaction deposit = new Deposit(amount);
+			checking.doTransaction(deposit);
+		}
+		else if(s == "Savings"){
+			MonetaryTransaction deposit = new Deposit(amount);
+			savings.doTransaction(deposit);
+		}
+		else if(s == "Retirement"){
+			MonetaryTransaction deposit = new Deposit(amount);
+			retirement.doTransaction(deposit);
+		}
+
+		//If a string was returned, say so.
+//		if ((s != null) && (s.length() > 0)) {
+//		    setLabel("Green eggs and... " + s + "!");
+//		    return;
+//		}
+//
+//		//If you're here, the return value was null/empty.
+//		setLabel("Come on, finish the sentence!");
 	}
 }
